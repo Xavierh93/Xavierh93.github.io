f3ca5f5c9f27644699897f8f7c8a35bc28060493
matt <koenimat@oregonstate.edu>
2019-11-01 23:13:55 -0400

    Remove unused images and css files, updated landing page

--- Home.js
+++ Home.js
@@ -10,7 +10,7 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) 
-  	{slideIndex = 1} 
+    {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
   setTimeout(showSlides, 2000); 
 }
