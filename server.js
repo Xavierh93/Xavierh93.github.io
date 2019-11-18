786be8d61e5121101a9fb872bc6a398be2e06c19
Xavier Hollingswort <xavierh93@hotmail.com>
2019-11-18 17:49:05 -0500

    update

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: process.env.DBUSER,
+	password: process.env.DBPASS,
+	database: process.env.DBNAME
+});
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+
+
+app.use(function(req, res) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.log(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Application started on port ' + app.get('port'));
+});
